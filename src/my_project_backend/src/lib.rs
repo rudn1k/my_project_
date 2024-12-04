cdcbff501bfb6220565c57501c921e6152f33378
Bartosz Rudnicki <bartekrud3@gmail.com>
2024-12-04 09:39:28 +0000

    Dodano opcje zapisu wiadomości

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,20 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static MSG: RefCell<String> = RefCell::new(String::new());
+}
+
+#[ic_cdk::update]
+fn set_msg(new_msg: String) {
+    // COUNTER.with(|counter| (*counter.borrow()).clone())
+
+    MSG.with(|msg|{
+        *msg.borrow_mut() = new_msg;
+    })
+}
+
+
 #[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+fn get_msg() -> String {
+MSG.with(|msg| msg.borrow().clone())
 }
